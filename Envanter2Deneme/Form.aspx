143f28b4ddba6ca0df0c3df280cb477d48aa5680
Ahmet Raşit İlhan <arasitilhan@hotmail.com>
2019-12-01 18:20:11 +0300

    first commit

---
+++ Envanter2Deneme/Form.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Form.aspx.cs" Inherits="Envanter2Deneme.Form" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Envanter 2 - Çözüm</title>
+    <link href="GenelCss.css" rel="stylesheet" type="text/css"/>
+    <script type="text/javascript" charset="utf-8" src="app.js"></script>
+</head>
+<body>
+    
+        <div style="text-align:center;"><button id="btn_yukletara">Yükleme ve Taramayı Başlat</button></div>
+        <div id="sayacID" style="text-align:center;"></div>
+        <div>Progress:</div>
+        <div id="myProgress">
+            <div id="myBar"></div>
+        </div><br/>
+        <div>Yükleme:</div>
+        <div id="myProgressYukleme">
+            <div id="myBarYukleme"></div>
+        </div><br/>
+        <div>Tarama:</div>
+        <div id="myProgressTarama">
+            <div id="myBarTarama"></div>
+        </div>
+        <div>
+            <table>
+                <tr><th>Alert No</th><th>Öncelik</th><th>Eventler</th></tr>
+                <tr>
+                    <td id="alertno"></td>
+                    <td id="oncelik"></td>
+                    <td id="event"></td>
+                </tr>
+            </table> 
+        </div>
+
+</body>
+</html>
---
+++ Envanter2Deneme/app.js
@@ -0,0 +1,57 @@
+﻿window.onload = function () {
+    document.getElementById("btn_yukletara").addEventListener("click", function () {
+        var j = 0;
+        var alertsay = 0;
+        var events=[];
+        var prio = [];
+        var bar = 0;
+        if (bar == 0) {
+            bar = 1;
+            var elem = document.getElementById("myBar");
+            var elemYukleme = document.getElementById("myBarYukleme");
+            var elemTarama = document.getElementById("myBarTarama");
+            var sayac = document.getElementById("sayacID");
+            var width_Yukleme = 0;
+            var width_Tarama = 0;
+            var width = 0;
+            var id = 0;
+            id = setInterval(frame, 0);
+            setTimeout(function () {
+                for (var i = 0; i < 400; i++) {
+                    j++;
+                    events.push("E" + j);
+                    prio.push(Math.floor((Math.random() * 3) + 1));
+                    if (prio.length % 4 == 0) {
+                            width_Yukleme++;
+                            elemYukleme.style.width = width_Yukleme + "%";
+                    }               
+                }
+            }, 3000);
+            setTimeout(function () {
+                for (var index = 2; index < prio.length;index++) {
+                    if (prio[index] == prio[index - 1] && prio[index - 2] == prio[index]) {
+                            alertsay++;
+                            document.getElementById("alertno").innerHTML = document.getElementById("alertno").innerHTML + alertsay + "<br>";
+                            document.getElementById("oncelik").innerHTML = document.getElementById("oncelik").innerHTML + prio[index] + "<br>";
+                            document.getElementById("event").innerHTML = document.getElementById("event").innerHTML + events[index - 2] + "," + events[index - 1] + "," + events[index] + "<br>";
+                    }
+                    if ((index + 2) % 4 == 0) {
+                        width_Tarama++;
+                        elemTarama.style.width = width_Tarama + "%";
+                    }
+                }
+            }, 5000);
+            function frame() {
+                if (width >= 100) {
+                    clearInterval(id);
+                    bar = 0;
+                } else {
+                    width = (width_Yukleme + width_Tarama)/2;
+                    elem.style.width = width + "%";
+                    sayac.innerHTML = width + "%";
+                }
+            }
+            
+        }
+    });
+}
